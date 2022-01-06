f5b1df6cbd4e08875bb6d2be0d4fa73077d7a1da
Titouan <titouan.lacombe99@gmail.com>
2022-01-06 09:49:26 +0100

    bloop

--- scripts/AIController.js
+++ scripts/AIController.js
@@ -1,18 +1,20 @@
-class AIController extends Controller {
+class AIController extends Controller
+{
 	static vision_range = 500;
 
-	constructor() {
+	constructor()
+	{
 		super();
 
 		// --- DNA ---
-		this.min_fighter_dist = 50;
+		this.min_fighter_dist = 75;
 		this.max_fighter_dist = 400;
-		this.wanted_target_dist = 150;
+		this.wanted_target_dist = 200;
 
 		this.min_fire_error = 0.05;
 
-		this.encounter_time_max = 20;
-		this.encounter_dist_max = 5;
+		this.encounter_time_max = 10;
+		this.encounter_dist_max = 10;
 
 		this.positionning_K = 1;
 		this.searching_K = 1;
@@ -21,7 +23,7 @@ class AIController extends Controller {
 		this.angle_Kp = 0.3;
 		this.angle_Ki = 0.5;
 		this.angle_Kd = 0.3;
-		
+
 		this.vel_Kp = 0.3;
 		this.vel_Ki = 0.5;
 		this.vel_Kd = 0.3;
@@ -39,7 +41,8 @@ class AIController extends Controller {
 	}
 
 	// Can't be static function because the exit condition changes depending on the AI DNA
-	init_states() {
+	init_states()
+	{
 		// let suicide = new State("suicide");
 		let searching = new State("searching");
 		let aiming = new State("aiming");
@@ -49,7 +52,8 @@ class AIController extends Controller {
 
 		// --- "aiming" state ---
 		// Switch to "positionning" if target not at good distance
-		aiming.add_exit(positionning, (object) => {
+		aiming.add_exit(positionning, (object) =>
+		{
 			if (!this.target) {
 				return false;
 			}
@@ -59,61 +63,71 @@ class AIController extends Controller {
 		});
 
 		// Switch to "searching" if has no target
-		aiming.add_exit(searching, (object) => {
-			return object.controller.target == null;
+		aiming.add_exit(searching, (object) =>
+		{
+			return this.target == null;
 		});
 
 		// Switch to "turret" if has no fuel
-		aiming.add_exit(turret, (object) => {
+		aiming.add_exit(turret, (object) =>
+		{
 			return object.fuel <= 0;
 		});
 
 		// Switch to "fleeing" if has no munitions
-		aiming.add_exit(fleeing, (object) => {
+		aiming.add_exit(fleeing, (object) =>
+		{
 			return object.munitions <= 0;
 		});
 
 		// --- "positionning" state ---
 		// Switch to "aiming" if target is in range
-		positionning.add_exit(aiming, (object) => {
-			if (!object.controller.target) {
+		positionning.add_exit(aiming, (object) =>
+		{
+			if (!this.target) {
 				return false;
 			}
-			let d = object.controller.target.dist_to(object);
-			return d < this.max_fighter_dist && d > this.min_fighter_dist;
+			let d = this.target.dist_to(object);
+			return d < this.max_fighter_dist && d > (this.min_fighter_dist + 50);
 		});
 
 		//	Switch to "searching" if has no target
-		positionning.add_exit(searching, (object) => {
-			return object.controller.target == null;
+		positionning.add_exit(searching, (object) =>
+		{
+			return this.target == null;
 		});
 
 		// Switch to "turret" if has no fuel
-		positionning.add_exit(turret, (object) => {
+		positionning.add_exit(turret, (object) =>
+		{
 			return object.fuel <= 0;
 		});
 
 		// Switch to "fleeing" if has no munitions
-		positionning.add_exit(fleeing, (object) => {
+		positionning.add_exit(fleeing, (object) =>
+		{
 			return object.munitions <= 0;
 		});
 
 		// --- "searching" state ---
 		// Switch to "positionning" when finds a target
-		searching.add_exit(positionning, (object) => {
-			return object.controller.target != null;
+		searching.add_exit(positionning, (object) =>
+		{
+			return this.target != null;
 		});
 
 		this.state = searching;
 	}
 
-	get_angle(object) {
+	get_angle(object)
+	{
 		return object.angle - Math.PI / 2;
 	}
-	
+
 	// Choose closest fighter
 	// Bias via ones in front (TODO)
-	find_target(object, near_by_objects) {
+	find_target(object, near_by_objects)
+	{
 		let min_distance = Infinity;
 		for (let potential of near_by_objects) {
 			if (potential instanceof Fighter) {
@@ -126,11 +140,12 @@ class AIController extends Controller {
 		}
 	}
 
-	manage_target(object, near_by_objects) {
+	manage_target(object, near_by_objects)
+	{
 		// Loose target if => too far or dead
 		if (this.target) {
 			if (!this.target.alive ||
-				this.target.dist_to(object) > AIController.vision_range) {
+				this.target.dist_to(object) > this.max_fighter_dist) {
 				this.target = null;
 			}
 		}
@@ -141,7 +156,8 @@ class AIController extends Controller {
 		}
 	}
 
-	get_firering_angle(object) {
+	get_firering_angle(object)
+	{
 		let result = TrajectoryPredictor.get_firering_angle(object, this.target, Fighter.fire_vel);
 
 		if (result != null) {
@@ -151,37 +167,40 @@ class AIController extends Controller {
 
 			// Draw angle
 			// Vector2.fromAngle(result.angle).draw(ctx, Color.red, 30);
-			
+
 			// console.log(target_pos);
 			// console.log(result.dt);
 			// console.log(result.angle);
-			
+
 			// Draw red point at target x bullet intersection
 			ctx.beginPath();
 			ctx.arc(target_pos.x, target_pos.y, 5, 0, 2 * Math.PI);
 			ctx.fill();
+		} else {
+			console.log("Warning: no fireing angle solutions found");
 		}
 
 		return result.angle;
 	}
 
 	// fire if current aim close enough to targeted aim && cooldown passed
-	do_fire(current_angle, target_angle) {
+	do_fire(current_angle, target_angle)
+	{
 		return Math.abs(current_angle - target_angle) < this.min_fire_error;
 	}
 
-	get_evading_vector(object, near_by_objects) {
+	get_evading_vector(object, near_by_objects)
+	{
 		let evading_v = new Vector2();
 
 		for (const obj of near_by_objects) {
 			let encounter = TrajectoryPredictor.get_encounter(object, obj);
 			let dist = encounter.pos1.clone().sub(encounter.pos2);
 
-			if (encounter.dt < this.encounter_time_max
-				&& dist.norm() - obj.radius - object.radius < this.encounter_dist_max)
-			{
+			if (encounter.dt < this.encounter_time_max &&
+				dist.norm() - obj.radius - object.radius < this.encounter_dist_max) {
 				// Evade
-				let score = 1;
+				let score = 1 / (encounter.dt + dist.norm() + 1);
 				evading_v.add(dist.mul(score));
 			}
 		}
@@ -189,7 +208,8 @@ class AIController extends Controller {
 		return evading_v;
 	}
 
-	control_from_vector(object, speed_target) {
+	control_from_vector(object, speed_target)
+	{
 		let object_angle = this.get_angle(object);
 
 		let target_angle = speed_target.angle();
@@ -203,7 +223,8 @@ class AIController extends Controller {
 		};
 	}
 
-	searching(object, near_by_objects) {
+	searching(object, near_by_objects)
+	{
 		let speed_target = new Vector2();
 
 		// Go to center
@@ -215,34 +236,39 @@ class AIController extends Controller {
 		return this.control_from_vector(object, speed_target);
 	}
 
-	positionning(object, near_by_objects) {
+	positionning(object, near_by_objects)
+	{
 		let speed_target = new Vector2();
-		
+
 		// Constrain target distance to object
 		let diff_pos = this.target.pos.clone().sub(object.pos);
 		let target_pos = diff_pos.clone().normalize(this.wanted_target_dist);
 		let error = diff_pos.sub(target_pos);
-		// error.draw(ctx, "red", 1);
 
 		speed_target.add(error.mul(this.positionning_K));
-
 		speed_target.add(this.get_evading_vector(object, near_by_objects));
-		
+
 		return this.control_from_vector(object, speed_target);
 	}
 
-	aiming(object, near_by_objects) {
+	aiming(object, near_by_objects)
+	{
 		let target_angle = this.get_firering_angle(object);
 		let object_angle = this.get_angle(object);
 
+		// Evade only impacts thrust
+		let speed_target = this.get_evading_vector(object, near_by_objects);
+		let evade_control = this.control_from_vector(object, speed_target);
+
 		return {
-			"thrust": 1, // Min thrust
+			"thrust": constrain_value(evade_control.thrust, 0.2, 1),
 			"rotation": this.angle_pid.control(object_angle, target_angle, dt),
 			"fire": this.do_fire(object_angle, target_angle),
 		};
 	}
 
-	turret(object, near_by_objects) {
+	turret(object, near_by_objects)
+	{
 		let target_angle = this.get_firering_angle(object);
 		let object_angle = this.get_angle(object);
 
@@ -252,7 +278,8 @@ class AIController extends Controller {
 		};
 	}
 
-	fleeing(object, near_by_objects) {
+	fleeing(object, near_by_objects)
+	{
 		let speed_target = new Vector2();
 
 		speed_target.add(this.get_evading_vector(object, near_by_objects));
@@ -260,8 +287,9 @@ class AIController extends Controller {
 		return this.control_from_vector(object, speed_target);
 	}
 
-	change_state(new_state) {
-		// console.log("Changing state to: " + this.state.code);
+	change_state(new_state)
+	{
+		// console.log("Changing state to: " + new_state.code);
 
 		this.state = new_state;
 
@@ -269,7 +297,8 @@ class AIController extends Controller {
 		this.angle_pid.reset_prev();
 	}
 
-	control(_object) {
+	control(_object)
+	{
 		const object = _object;
 
 		// Debug drawing context
@@ -277,7 +306,7 @@ class AIController extends Controller {
 		ctx.strokeStyle = "red";
 		ctx.fillStyle = "red";
 		ctx.lineWidth = 2;
-		
+
 		// Manage target
 		let near_by_objects = CollisionManager.get_near_objects(object, AIController.vision_range, "space");
 		this.manage_target(object, near_by_objects);
@@ -288,33 +317,18 @@ class AIController extends Controller {
 			this.change_state(new_state);
 		}
 
+		// Generate command
 		let command = {};
-		switch (this.state.code) {
-			case "searching":
-				command = this.searching(object, near_by_objects);
-				break;
 
-			case "positionning":
-				command = this.positionning(object, near_by_objects);
-				break;
-
-			case "aiming":
-				command = this.aiming(object, near_by_objects);
-				break;
-
-			case "turret":
-				command = this.turret(object, near_by_objects);
-				break;
-
-			case "fleeing":
-				command = this.fleeing(object, near_by_objects);
-				break;
-
-			default:
-				console.log("Unknown state: " + this.state.code);
-				break;
+		try {
+			command = this[this.state.code](object, near_by_objects);
+		}
+		catch (error) {
+			console.log(error);
+			console.log("Unknown state: " + this.state.code);
 		}
 
+		// Reset debug drawing context
 		ctx.resetTransform();
 
 		return command;
