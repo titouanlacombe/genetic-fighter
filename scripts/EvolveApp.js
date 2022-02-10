d3e3aa0bddeaa21d73f23e7b2b40c255edf74dd9
Nathan BARBIER <nathang.barbier@gmail.com>
2022-02-10 13:29:27 +0100

    push

--- scripts/EvolveApp.js
+++ scripts/EvolveApp.js
@@ -1,11 +1,23 @@
+/**
+ * Application to manage initialization and 
+ * evolution of the game and the population
+ * @extends Game
+ */
 class EvolveApp extends Game {
-    constructor() {
+    /**
+     * @constructor
+     */
+    constructor(target_generation) {
         super();
+        this.evolve = new EvolutionManager(0.1, 5);
+        this.target_generation = target_generation;
     }
 
+    /**
+     * Spawns objets (AI)
+     */
     initing() {
         console.log("Initialization");
-        this.evolve = new EvolutionManager(0.1, 5);
         this.objects = [];
 
         console.log(this.evolve.size);
@@ -16,15 +28,21 @@ class EvolveApp extends Game {
             this.objects.push(f);
         }
 
-        console.log(this.objects);
+        // console.log(this.objects);
     }
 
-    update() {
-        super.update();
-    }
+    // update() {
+    //     super.update();
+    // }
 
+
+    /**
+     * Exit when the is a winner or all objects are dead
+     * Then create a new generation of objects
+     */
     exiting() {
         super.exiting();
-        console.log("Winner in exit (): ", this.winner);
+        // download generation's DNA
+        // start new generation if generation < target_generation
     }
 }
