667a8323f240e814d8b4dc8d93b99640b1d0e268
Titouan <titouan.lacombe99@gmail.com>
2022-02-10 17:07:54 +0100

    bloop

--- scripts/Framework.js
+++ scripts/Framework.js
@@ -138,4 +138,4 @@ class Framework {
 	}
 }
 
-let framework = new Framework();
+const framework = new Framework();
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,2 @@
 // Launch application
-const framework = new Framework();
 framework.start(new EvolveApp());
